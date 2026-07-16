562be8e78857f1f28c6b57c734a9a5dc9031b27c
oevery <i@oevery.me>
2026-07-16 14:41:31 +0800

    fix(darwin): 完善 Zed 文件关联与 Dock 集成

--- modules/darwin/gui/desktop.nix
+++ modules/darwin/gui/desktop.nix
@@ -11,6 +11,22 @@ let
     "/System/Applications/System Settings.app"
     "/Applications/Google Chrome.app"
     "/Applications/Zed.app"
+    "/Applications/Warp.app"
+    "/Applications/ChatGPT.app"
+  ];
+
+  zedDynamicFileExtensions = [
+    "jsonc"
+    "nix"
+    "cjs"
+    "jsx"
+    "cts"
+    "vue"
+    "svelte"
+    "astro"
+    "scss"
+    "rs"
+    "go"
   ];
 
   zedFileExtensions = [
@@ -39,6 +55,69 @@ let
     "go"
     "java"
   ];
+
+  zedFileTypesInfo = pkgs.writeText "zed-file-types-info.plist" ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>CFBundleDocumentTypes</key>
+      <array>
+        <dict>
+          <key>CFBundleTypeName</key>
+          <string>Zed source code</string>
+          <key>CFBundleTypeRole</key>
+          <string>Editor</string>
+          <key>LSHandlerRank</key>
+          <string>Alternate</string>
+          <key>LSItemContentTypes</key>
+          <array>
+            <string>io.github.oevery.zed-source-code</string>
+          </array>
+        </dict>
+      </array>
+      <key>CFBundleExecutable</key>
+      <string>ZedFileTypes</string>
+      <key>CFBundleIdentifier</key>
+      <string>io.github.oevery.zed-file-types</string>
+      <key>CFBundleName</key>
+      <string>ZedFileTypes</string>
+      <key>CFBundlePackageType</key>
+      <string>APPL</string>
+      <key>CFBundleVersion</key>
+      <string>1</string>
+      <key>UTExportedTypeDeclarations</key>
+      <array>
+        <dict>
+          <key>UTTypeConformsTo</key>
+          <array>
+            <string>public.source-code</string>
+            <string>public.text</string>
+          </array>
+          <key>UTTypeDescription</key>
+          <string>Zed source code</string>
+          <key>UTTypeIdentifier</key>
+          <string>io.github.oevery.zed-source-code</string>
+          <key>UTTypeTagSpecification</key>
+          <dict>
+            <key>public.filename-extension</key>
+            <array>
+    ${lib.concatMapStringsSep "\n" (
+      extension: "              <string>${extension}</string>"
+    ) zedDynamicFileExtensions}
+            </array>
+          </dict>
+        </dict>
+      </array>
+    </dict>
+    </plist>
+  '';
+
+  zedFileTypesApp = pkgs.runCommand "zed-file-types.app" { } ''
+    mkdir -p "$out/Contents/MacOS"
+    cp ${zedFileTypesInfo} "$out/Contents/Info.plist"
+    cp ${pkgs.coreutils}/bin/true "$out/Contents/MacOS/ZedFileTypes"
+  '';
 in
 
 lib.mkIf pkgs.stdenv.isDarwin {
@@ -68,6 +147,8 @@ lib.mkIf pkgs.stdenv.isDarwin {
 
   # Homebrew cask 会先于 Home Manager 激活，因此此时 Zed 已可供 Launch Services 使用。
   home.activation.setZedFileAssociations = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister \
+      -f "${zedFileTypesApp}"
     for extension in ${lib.escapeShellArgs zedFileExtensions}; do
       run ${pkgs.duti}/bin/duti -s dev.zed.Zed ".$extension" all
     done
